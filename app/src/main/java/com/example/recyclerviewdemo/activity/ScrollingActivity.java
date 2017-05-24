c7e75db660e6b43ba119fe19ada15995a306dcee
Steven Wang <hashtable@163.com>
2017-05-24 23:32:40 +0800

    Scrolling with RecyclerView

--- app/src/main/java/com/example/recyclerviewdemo/activity/ScrollingActivity.java
+++ app/src/main/java/com/example/recyclerviewdemo/activity/ScrollingActivity.java
@@ -4,21 +4,45 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.recyclerviewdemo.R;
+import com.example.recyclerviewdemo.adapter.MultipleItemAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    private RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
+        initView();
+    }
+
+    private void initView() {
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));//这里用线性显示 类似于listview
+        recyclerView.setAdapter(mAdapter = new MultipleItemAdapter(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
