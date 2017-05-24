caa6f83fd58fb03dccf5412f7cd92aef0cecfd52
wangyuhang <wangyuhang@le.com>
2017-05-24 16:17:07 +0800

    下拉刷新RecylerView

--- app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -1,24 +1,19 @@
 package com.example.recyclerviewdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import it.gmariotti.recyclerview.adapter.AlphaAnimatorAdapter;
-import it.gmariotti.recyclerview.adapter.ScaleInAnimatorAdapter;
-import it.gmariotti.recyclerview.adapter.SlideInBottomAnimatorAdapter;
-import it.gmariotti.recyclerview.itemanimator.ScaleInOutItemAnimator;
-import it.gmariotti.recyclerview.itemanimator.SlideInOutLeftItemAnimator;
-import it.gmariotti.recyclerview.itemanimator.SlideScaleInOutRightItemAnimator;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.recycler_view)
@@ -27,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     Button addBtn;
     @BindView(R.id.removeBtn)
     Button removeBtn;
+    @BindView(R.id.swipe_layout)
+    SwipeRefreshLayout swipeLayout;
 
     private RecyclerView.Adapter mAdapter;
 
@@ -36,24 +33,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+//        startActivity(new Intent(MainActivity.this, RefreshActivity.class));
+
+        initView();
+    }
+
+    private void initView() {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));//这里用线性显示 类似于listview
 //        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));//这里用线性宫格显示 类似于grid view
-//        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL));//这里用线性宫格显示 类似于瀑布流
+//        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL));// 这里用线性宫格显示 类似于瀑布流
 
 //        mRecyclerView.setAdapter(new NormalRecyclerViewAdapter(this));
         mRecyclerView.setAdapter(mAdapter = new MultipleItemAdapter(this));
 
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());//动画效果，更多效果使用参考：https://github.com/gabrielemariotti/RecyclerViewItemAnimators
+
+        swipeLayout.setDistanceToTriggerSync(666);//设定手指下滑距离出发刷新
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mAdapter.notifyDataSetChanged();
+                swipeLayout.setRefreshing(false);
+            }
+        });
     }
 
     @OnClick({R.id.addBtn, R.id.removeBtn})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.addBtn:
-                ((MultipleItemAdapter)mAdapter).addData(0);
+                ((MultipleItemAdapter) mAdapter).addData(0);
                 break;
             case R.id.removeBtn:
-                ((MultipleItemAdapter)mAdapter).removeData(0);
+                ((MultipleItemAdapter) mAdapter).removeData(0);
                 break;
         }
     }
